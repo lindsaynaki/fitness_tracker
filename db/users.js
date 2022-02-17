0f33c19128f5c66aaf3f0a2ca53b7fc5fbd48efd
alancuenca <alan.r.cuenca@gmail.com>
2022-02-16 20:47:37 -0800

    mod to users

--- db/users.js
+++ db/users.js
@@ -1,1 +1,19 @@
 const { client } = require('./client');
+
+const createUser = async ({username, password}) => {
+    try {
+        const { rows: [user] } = await client.query(`
+            INSERT INTO users (username, password)
+            VALUES ($1, $2)
+            RETURNING *; 
+        `, [username, password]);
+        delete user.password
+        return user;
+    } catch(error) {
+        throw error; 
+    }
+}
+
+module.exports = {
+    createUser
+}
