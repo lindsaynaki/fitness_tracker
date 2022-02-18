1da52631d803e18465d858fee20b510bc2523a2a
lindsaynaki <87101797+lindsaynaki@users.noreply.github.com>
2022-02-17 19:45:31 -0800

    Revert "hashed passwords"

--- db/users.js
+++ db/users.js
@@ -1,42 +1,27 @@
-const client = require('./client');
-const bcrypt = require('bcrypt');
-const SALT_ROUNDS = 10;
+const  client  = require('./client');
 
 const createUser = async ({username, password}) => {
-    const hashPwd = await bcrypt.hash(password, SALT_ROUNDS);
     try {
         const { rows: [user] } = await client.query(`
             INSERT INTO users (username, password)
             VALUES ($1, $2)
             RETURNING *; 
-        `, [username, hashPwd]);
+        `, [username, password]);
+        delete user.password
         return user;
     } catch(error) {
         throw error; 
     }
 };
 
-const getUser = async ({ username, password }) => {
+const getUser = async ({ username }) => {
     try{
         const {rows: [user] } = await client.query(`
-        SELECT * FROM users
-        WHERE username = $1
+        SELECT *
+        FROM users
+        WHERE username=$1
         `, [username])
-
-        if (!user) {
-            throw {
-                name: "UserNotFound",
-                message: "User not found with that username"
-            }
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password)
-
-        if (isMatch) {
-            delete user.password;
-            return user
-        }
-
+        return user
     } catch(error){
         throw error;
     }
