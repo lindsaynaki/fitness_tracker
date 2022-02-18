7df55ca57d79420aaf3a250676c7bbb3fff56888
Lindsay Naki <lindsnakamura@gmail.com>
2022-02-17 19:51:10 -0800

    updated bcrypt in users

--- db/users.js
+++ db/users.js
@@ -1,27 +1,42 @@
 const  client  = require('./client');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 
 const createUser = async ({username, password}) => {
+    const hashPwd = await bcrypt.hash(password, SALT_ROUNDS);
     try {
         const { rows: [user] } = await client.query(`
             INSERT INTO users (username, password)
             VALUES ($1, $2)
             RETURNING *; 
-        `, [username, password]);
-        delete user.password
+        `, [username, hashPwd]);
         return user;
     } catch(error) {
         throw error; 
     }
 };
 
-const getUser = async ({ username }) => {
+const getUser = async ({ username, password }) => {
     try{
         const {rows: [user] } = await client.query(`
-        SELECT *
-        FROM users
-        WHERE username=$1
+        SELECT * FROM users
+        WHERE username = $1
         `, [username])
-        return user
+
+        if (!user) {
+            throw {
+                name: "UserNotFound",
+                message: "User not found with that username"
+            }
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            delete user.password;
+            return user
+        }
+
     } catch(error){
         throw error;
     }
